a8bedeb1eb5e2c48a9063cc5688d519f12c8552c
Nicole Wakio <nicole.mulela@strathmore.edu>
2021-08-23 09:54:37 +0300

    Create product implementation update. image storing process update pending

--- seller/newproduct.php
+++ seller/newproduct.php
@@ -3,40 +3,54 @@
 <?php session_start(); ?>
 
 <div class="container">
-  <form action="processes/newproductprocess.php" method='post'>
+  <form action="processes/newproductprocess.php" method='post' enctype="multipart/form-data">
     <H3>Create New Product</H3>
     <div class="col-6">
       <label for="ProductName" class="form-label">Product Name</label>
       <input type="text" class="form-control" name="ProductName" id="ProductName" placeholder="Product Name...">
     </div>
-    <div class="col-6">
-      <label for="ProductDescription" class="form-label">Description</label>
-      <input type="text" class="form-control" name="ProductDescription" id="itemDescription" cols="30" rows="10" placeholder="Product description...">
+    <div class="col-md-6">
+      <label for="ProductCategory" class="form-label">Product Category</label>
+      <select class="form-select" name='ProductCategory' id="ProductCategory">
+      <option selected>Select a Category</option>
+      <?php
+      //get categories
+      $get_cats="SELECT * from product_category";
+      $run_cats= mysqli_query($conn, $get_cats);
+
+      while($row_cats=mysqli_fetch_array($run_cats)){
+        $cat_id= $row_cats['CategoryID'];
+        $cat_title=$row_cats['CategoryName'];
+        echo "<option value='$cat_id' >$cat_title</option>";
+      }
+      ?>
+    </select>
     </div>
+    <!--Not yet functional. Transfer of the image into the database -->
     <div class="col-6">
-      <label for="ProductManufacturer" class="form-label">Manufacturer</label>
-      <input type="text" class="form-control" name="ProductManufacturer" id="ProductManufacturer" placeholder="Manufacturer">
+      <label for="ProductImage" class="form-label">Product Image</label>
+      <input type="file" class="form-control" name="ProductImage" id="ProductImage" placeholder="Manufacturer">
     </div>
-    <div class="col-6">
-      <label for="ProductQuantity" class="form-label">Quantity</label>
-      <input type="number" class="form-control" name="ProductQuantity" id="ProductQuantity" placeholder="0">
-    </div>
+    
     <div class="col-6">
       <label for="ProductPrice" class="form-label">Unit Price</label>
       <input type="number" class="form-control" name="ProductPrice" id="ProductPrice" placeholder="0.00">
     </div>
-    <!--not yet functional -->
-    <div class="col-md-6">
-      <label for="ProductCategory" class="form-label">Category</label>
-      <select class="form-select" id="ProductCategory">
-      <option selected>Choose...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </select>
+    <div class="col-6">
+      <label for="ProductQuantity" class="form-label">Product Quantity</label>
+      <input type="number" class="form-control" name="ProductQuantity" id="ProductQuantity" placeholder="0">
     </div>
     <div class="col-6">
-      <button type="submit" name="add" class="btn btn-primary">Add Item</button>
+      <label for="ProductManufacturer" class="form-label">Product Manufacturer</label>
+      <input type="text" class="form-control" name="ProductManufacturer" id="ProductManufacturer" placeholder="Manufacturer">
+    </div>
+    <div class="col-6">
+      <label for="ProductDescription" class="form-label">Product Description</label>
+      <textarea type="text" class="form-control" name="ProductDescription" id="itemDescription" cols="20" rows="10" placeholder="Product description..."></textarea>
+    </div>
+    
+    <div class="col-6">
+      <button type="submit" name="add_product" class="btn btn-primary">Add Item</button>
     </div>
   </form>
 </div>
--- seller/processes/newproductprocess.php
+++ seller/processes/newproductprocess.php
@@ -1,23 +1,30 @@
 <?php
 require_once("dbconnect.php");
-
-if(isset($_POST['add']))
+session_start();
+if(isset($_POST['add_product']))
 { 
     $productName=$_POST["ProductName"];
     $productDescription=$_POST["ProductDescription"];
     $productManufacturer=$_POST["ProductManufacturer"];
     $productQuantity=$_POST["ProductQuantity"];
     $productPrice=$_POST['ProductPrice'];
+    $productCategory=$_POST['ProductCategory'];
+    
+    //Getting the image from the field
+    $productImage= $_FILES['ProductImage']['name'];
+    $productImageTmp= $_FILES['ProductImage']['tmp_name'];
+
+    move_uploaded_file($productImage, "images/product_images/$productImageTmp");
 
     $id=$_SESSION['UserID'];
-    $qry=" INSERT INTO product(ProductName,ProductDescription,ProductPrice,ProductQuantity,ProductManufacturer,UserID,CategoryID ) 
-    VALUES ('$productName','$productDescription','$productPrice','$productQuantity','$productManufacturer', '1','1')";
+    $qry=" INSERT INTO product(ProductName,ProductDescription,ProductPrice,ProductQuantity,ProductManufacturer,UserID,CategoryID, 
+    ProductImage ) VALUES ('$productName','$productDescription','$productPrice','$productQuantity','$productManufacturer', 
+    '$id','$productCategory','$productImage')";
+
     if (mysqli_query($conn,$qry)==TRUE)
     {
         echo '<script> alert("Successful");</script>';
         session_start();
-        $_SESSION['User']=$un;
-            echo ("login successful. Welcome").$_SESSION['User'];
             $url=("../product.php");
             header("location: $url");
     }
