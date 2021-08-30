223098f6b0238bb0a5e6281e6a6b6611ea3a2f3b
Nicole Wakio <nicole.mulela@strathmore.edu>
2021-08-30 20:14:54 +0300

    Update Seller view and edit profile functionality

--- seller/includes/header.inc.php
+++ seller/includes/header.inc.php
@@ -1,3 +1,5 @@
+<?php require_once("processes/dbconnect.php"); ?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +9,8 @@
     <title>Seller Portal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <!--style sheets-->
+    <link rel="stylesheet" href="../styles/client/main.css">
     
 </head>
 <body>
