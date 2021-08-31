49ae035db5fa2517fc4e293ec1fb379e41e5df0a
Nicole Wakio <nicole.mulela@strathmore.edu>
2021-08-31 13:22:21 +0300

    Seller side order functionality complete

--- seller/order.php
+++ seller/order.php
@@ -1,7 +1,6 @@
 <?php require_once("processes/dbconnect.php"); ?>
 <?php include_once 'includes/header.inc.php' ?>
 <div class="container">
-<?php session_start(); ?>
 <section>
 <?php
   /*all orders
@@ -10,70 +9,75 @@
     $id=$_SESSION['UserID'];
     $sql= "SELECT * FROM order_details INNER JOIN product ON order_details.ProductID = product.ProductID WHERE UserID='$id' ";
     $result = mysqli_query($conn, $sql);
-  ?>
-<h1>Recieved Orders</h1>
-  <table class="table table-hover table-sm">
-    <!-- PHP CODE TO FETCH DATA FROM ROWS-->
-    <?php   // LOOP TILL END OF DATA 
-            if (mysqli_num_rows($result) > 0) {//show table only if there are any products
-            ?>
-    <thead>
-    
-      <tr>
-        <th scope="col">Order Number</th>
-        <th scope="col">Order ID</th>
-        <th scope="col">Product ID</th>
-        <th scope="col">Quantity</th>
-        <th scope="col">Status</th>
-        <th scope="col">Payment</th>
-        <th scope="col" colspan="2" >Actions</th><!-- change status-->
-        <!--<th scope="col">Cancelled Date</th>
-        <th scope="col">Delivered Date</th>
--->
-      </tr>
-    </thead>
-    <tbody>
-    <?php   
-            $count=1; 
-                while($rows = mysqli_fetch_assoc($result))
-                {
-             ?>
-      <tr>
-         <!--FETCHING DATA FROM EACH ROW OF EVERY COLUMN-->
-         <td><?php echo $count; ?></td>
-                    <td><?php echo $rows['OrderDetailsID'];?></td>
-                    <td><?php echo $rows['ProductID'];?></td>
-                    <td><?php echo $rows['OrderQuantity'];?></td>
-                    <td><?php echo $rows['OrderStatus'];?></td>
-                    <td><?php echo $rows['OrderPayment'];?></td>
-                    <?php
-                    echo "
-                    <td>
-                        <a href='orderdetails.php?id=$rows[OrderDetailsID]'>Details</a>
-                    </td>
-                    ";
-                    ?>
-                    <td><select class="form-select" aria-label="Default select example">
-                      <option selected disabled>Status</option>
-                      <option value="1">Delivered</option>
-                      <option value="2">Cancelled</option>
-                    </select>
-                </td>
-      </tr>
-      <?php $count++;  ?>
-            
-            <?php
-                    }
-            ?>
-    </tbody>
-  </table>
-</div>
-<?php
+?>
+<div class="card mt-4">
+  <h5 class="card-header">Your Orders</h5>
+  <div class="card-body">
+    <table class="table table-hover table-sm">
+      <!-- PHP CODE TO FETCH DATA FROM ROWS-->
+      <?php   // LOOP TILL END OF DATA 
+              if (mysqli_num_rows($result) > 0) {//show table only if there are any products
+      ?>
+      <thead>
+      
+        <tr>
+          <th scope="col">Order Number</th>
+          <th scope="col">Order ID</th>
+          <th scope="col">Product</th>
+          <th scope="col">Quantity</th>
+          <th scope="col">Status</th>
+          <th scope="col">Payment</th>
+          <th scope="col" colspan="2" >Actions</th><!-- change status-->
+          <!--<th scope="col">Cancelled Date</th>
+          <th scope="col">Delivered Date</th>-->
+        </tr>
+      </thead>
+      <tbody>
+        <?php   
+              $count=1; 
+                  while($rows = mysqli_fetch_assoc($result))
+                  {
+                    //get product name
+                    $pID=$rows['ProductID'];
+                    $qry="SELECT * FROM product WHERE ProductID='$pID'";
+                    $get_product = mysqli_query($conn, $sql);
+                    $fetch_product= mysqli_fetch_assoc($get_product);
+
+          ?>
+        <tr>
+          <!--FETCHING DATA FROM EACH ROW OF EVERY COLUMN-->
+          <td><?php echo $count; ?></td>
+                      <td><?php echo $rows['OrderDetailsID'];?></td>
+                      <td><?php echo $fetch_product['ProductName'];?></td>
+                      <td><?php echo $rows['OrderQuantity'];?></td>
+                      <td><?php echo $rows['OrderStatus'];?></td>
+                      <td><?php echo $rows['OrderPayment'];?></td>
+                      <?php
+                      echo "
+                      <td>
+                          <a href='orderdetails.php?id=$rows[OrderDetailsID]'>Details</a>
+                      </td>
+                      ";
+                      ?>
+                      
+        </tr>
+        <?php $count++;  ?>
+              
+              <?php
+                      }
+              ?>
+      </tbody>
+    </table>
+    <?php
               } else {
                 echo "You don\'t have any orders yet";
               }
                 mysqli_close($conn); //close connection
-             ?>
+    ?>
+  </div>
+</div>
+
+  
 </section>
 <script>
 function myFunction() {
--- seller/orderdetails.php
+++ seller/orderdetails.php
@@ -1,44 +1,160 @@
 <?php require_once("processes/dbconnect.php"); ?>
 <?php include_once 'includes/header.inc.php' ?>
 <?php
+    //OrderDetailsID
     $id = $_GET['id'];
     
     $sql = "SELECT * FROM order_details WHERE OrderDetailsID= '$id'";
     $result = mysqli_query($conn, $sql);
 ?>
-<?php session_start(); ?>
+<?php
+//edit order status
+if(isset($_POST['edit_status']))
+{ 
+    //User ID
+    $id=$_SESSION['UserID'];
+    
+    $OrderStatus=$_POST["OrderStatus"];
+    
+    //update database
+    if($OrderStatus=='delivered'){
+        $sql = "UPDATE order_details SET OrderStatus='$OrderStatus', OrderPayment='paid' WHERE OrderDetailsID='$id'";
 
-<div class="container">
-    <h1>Order Details</h1>
-<div class="row">
-  <div class="col-md-6">
+    }elseif($OrderStatus=='cancelled'){
+        $sql = "UPDATE order_details SET OrderStatus='$OrderStatus', OrderPayment='not paid' WHERE OrderDetailsID='$id'";
 
-  <?php   
-    $count=1; 
-        while($rows = mysqli_fetch_assoc($result))
-        {
-        ?>
-            <h2>Order Item</h2>
-            <span>OrderDetailsID:</span> <?php echo $rows['OrderDetailsID']; ?><br>
-            <span>Product ID:</span> <?php echo $rows['ProductID']; ?><br>
-            <span>Product Name:</span> <br>
-            <span>Quantity:</span><br>
-            <span>Order Status:</span><br>
-            <span>OrderDate:</span>
-
-        <?php
+    }
+    
+    if (mysqli_query($conn,$sql)==TRUE)
+        {   
+            //Successful profile edit message
+            $_SESSION['message']="The Order has been ".$OrderStatus;
+            $_SESSION['msg_type']="success";
+            $url=("order.php");
+            header("location: $url");
+            
         }
-        ?>
-  </div>
-  <div class="col-md-6">
-      <h2>Shipping Details </h2>
-      <span>Name:</span><br>
-      <span>Contact:</span><br>
-      <span>Email:</span><br>
-      <span>Shipping Address:</span><br>
-      <hr>
+        else{
+            //Error Updating to DataBase
+            $_SESSION['message']="Error Updating to DataBase";
+            $_SESSION['msg_type']="danger";
+            $url=("order.php");
+            header("location: $url");
+        }
+      
+}
+?>
+<section class="vh-100 pt-4 container">
+    <?php
+    //session messages
+    if (isset($_SESSION['message'])):
+    ?>
 
-  </div>
-</div>
-</div>
-    <?php include_once 'includes/footer.inc.php' ?>
+    <div class="alert alert-<?=$_SESSION['msg_type']?>">
+    <?php
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    ?>
+    </div>
+    <?php endif; ?>  
+    <div class="row">
+        <div class=" col-8">
+            <div class="card">
+                <div class="card-header">
+                    Order Details
+                </div>
+                <div class="card-body">
+                    <?php   
+                        $count=1; 
+                        $rows = mysqli_fetch_array($result);
+                        
+                            //get product details
+                            $productID=$rows['ProductID'];
+                            $qry="SELECT * FROM product WHERE ProductID='$productID'";
+                            $get_product = mysqli_query($conn, $qry);
+                            $fetch_product= mysqli_fetch_assoc($get_product);
+
+                    ?>
+                    <div class="row">
+                        <div class="col">
+                            <img src="images/product_images/<?php echo $fetch_product['ProductImage']; ?>" width="250" height="auto">
+                        </div>
+                        <div class="col">
+                            <span>Order ID:</span> <?php echo $rows['OrderDetailsID']; ?><br>
+                            <span>Product:</span> <?php echo $fetch_product['ProductName']; ?> <br>
+                            <span>Product ID:</span> <?php echo $rows['ProductID']; ?><br>
+                            <span>Quantity:</span><?php echo $rows['OrderQuantity']; ?><br>
+                            <span>Order Status:</span> <?php echo $rows['OrderStatus']; ?><br>
+                            <!--<span>OrderDate:</span> -->
+                        </div>
+                    </div>
+                     
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-header">
+                    Shipping Address
+                </div>
+                <div class="card-body">
+                    <?php
+                        //get main OrderID
+                        $OrderID=$rows['OrderID'];
+
+                        //get ClientID from Orders table
+                        $query="SELECT * FROM orders WHERE OrderID='$OrderID'";
+                        $get_client = mysqli_query($conn, $query);
+                        $fetch_client=mysqli_fetch_array($get_client);
+                        $ClientID=$fetch_client['UserID'];
+
+                        //get shipping details
+                        $shipqry="SELECT * FROM shipping_details WHERE UserID='$ClientID'";
+                        $get_ship = mysqli_query($conn, $shipqry);
+                        $fetch_ship=mysqli_fetch_array($get_ship);
+                    ?>
+                    <span>Name: </span><?php echo $fetch_ship['FullName']; ?><br>
+                    <span>Contact: </span><?php echo $fetch_ship['Contact']; ?><br>
+                    <span>Address:</span><br><?php echo $fetch_ship['AddressDesc']; ?><br>
+                    <span>City: </span><?php echo $fetch_ship['City']; ?><br>
+
+
+                </div>
+            </div>
+            <form action="" method="post">
+                <div class="col-6 pt-4">
+                    <span>Change Order Status</span>
+                    <select class="form-select" name="OrderStatus">
+                        <?php
+                        if($rows['OrderStatus']=='pending'){
+                        ?>
+                        <option disabled>Pending</option>
+                        <option value="delivered">Delivered</option>
+                        <option value="cancelled">Cancelled</option>
+                        <?php
+                            }elseif($rows['OrderStatus']=='delivered'){
+                        ?>
+                        <option disabled>Pending</option>
+                        <option selected value="delivered">Delivered</option>
+                        <option value="cancelled">Cancelled</option>
+                        <?php
+                            }elseif($rows['OrderStatus']=='cancelled'){
+                        ?>
+                        <option disabled>Pending</option>
+                        <option value="delivered">Delivered</option>
+                        <option selected value="cancelled">Cancelled</option>
+                        <?php
+                            }
+                        ?>
+                    </select>
+                </div>
+                <button type="submit" name="edit_status" class="btn  btn-outline-primary m-4">Update</button>
+                <a href="order.php" class="btn  btn-outline-secondary m-4"> <span class="text">Back</span></a>
+            </form>
+        </div>
+        
+    </div>
+</section>
+
+<?php include_once 'includes/footer.inc.php' ?>
